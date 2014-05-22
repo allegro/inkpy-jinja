811b149b8619880b6243b785ebe8a80583dc63f0
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2014-05-22 12:19:46 +0200

    made var lang_code as a fn param

--- src/inkpy/api.py
+++ src/inkpy/api.py
@@ -7,15 +7,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import django_rq
-from django.conf import settings
 
 from inkpy import Converter
 from inkpy.utils import switch_language
 
 
-def generate_pdf(source_path, output_path, data):
+def generate_pdf(source_path, output_path, data, lang_code='en'):
     conv = Converter(source_path, output_path, data)
-    lang_code = getattr(settings, 'GENERATED_DOCS_LOCALE', 'en')
     with switch_language(lang_code):
         conv.convert()
 
