f684663fb1f199c65194c78424b5902e98de3dfe
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-06-06 13:48:40 +0200

    Render template in sandboxed environment
    Now jinja templates are rendered in sandboxed environment to secure evaluate untrusted templates. Access to unsafe attributes and methods is denied. See http://jinja.pocoo.org/docs/dev/sandbox/ for details.

--- inkpy_jinja/tests/tests.py
+++ inkpy_jinja/tests/tests.py
@@ -1,28 +1,33 @@
 # -*- coding: utf-8 -*-
+import os
 import unittest
 
-from inkpy_jinja.converter import Converter, FileDoesNotExist
+from jinja2.exceptions import SecurityError
+
+from inkpy_jinja.converter import Converter
+from inkpy_jinja.backends.libre import LibreOfficePDFBackend
+
+SAMPLES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'samples'))
+SAMPLE_TXT = os.path.join(SAMPLES_DIR, 'sample.txt')
+SAMPLE_NOT_SAFE_TXT = os.path.join(SAMPLES_DIR, 'sample_not_safe.txt')
 
 
 class ConverterTests(unittest.TestCase):
+    def _get_converter(self, data, file=SAMPLE_TXT):
+        return Converter(
+            SAMPLE_TXT, '', data,
+            backend=LibreOfficePDFBackend
+        )
 
-    def test_given_non_exist_template_should_raise_exception(self):
-        with self.assertRaises(FileDoesNotExist):
+    def test_jinja_renderer(self):
+        c = self._get_converter(data={'id': 123, 'test': '1234'})
+        result = c._jinja_renderer(open(SAMPLE_TXT, 'rb').read())
+        self.assertEqual(result, 'Lorem ipsum 1234')
 
-        # OK = 'Maj 31, 2014'
-        # source_file = output_path = 'unused_in_test'
-        # test_data = {
-        #     'id': 'mocked-id',
-        #     'today': datetime.date(2014, 5, 31),
-        # }
-        # converter = MockedConverter(source_file, output_path, test_data)
-        # file_content = "{{today}}"
-
-        # rendered = converter._django_renderer(file_content)
-        # self.assertNotIn('Maj', rendered)
-        # converter.lang_code = 'pl'
-        # rendered = converter._django_renderer(file_content)
-        # self.assertEqual(rendered, OK)
+    def test_jinja_renderer_not_safe(self):
+        c = self._get_converter(data={'id': 123})
+        with self.assertRaises(SecurityError):
+            c._jinja_renderer(open(SAMPLE_NOT_SAFE_TXT, 'rb').read())
 
 
 if __name__ == '__main__':
